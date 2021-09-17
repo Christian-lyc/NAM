14fd7f96450a62f10249d49865fae709ed7bd9c4
Yichao Liu <40708986+Christian-lyc@users.noreply.github.com>
2021-09-17 10:23:52 +0200

    Add files via upload

---
+++ train_cifar100.py
@@ -0,0 +1,357 @@
+import argparse
+import os
+import shutil
+import time
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+from MODELS.model_resnet import *
+from PIL import ImageFile
+from thop import profile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR', help='path to dataset')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet',help='model architecture: ' +' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('--depth', default=50, type=int, metavar='D', help='model depth')
+parser.add_argument('--ngpu', default=4, type=int, metavar='G', help='number of gpus to use')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=200, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int, metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float, metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=100, type=int,metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument("--seed", type=int, default=1234, metavar='BS', help='input batch size for training (default: 64)')
+parser.add_argument("--prefix", type=str, required=True, metavar='PFX', help='prefix for logging & checkpoint saving')
+parser.add_argument('--evaluate', dest='evaluate', action='store_true', help='evaluation only')
+parser.add_argument('--att-type', type=str, choices=['BAM', 'CBAM','NBAM'], default=None)
+parser.add_argument('--milestones',type=list,default=[60, 120, 160],help='optimizer milestones')
+parser.add_argument('--set', type=str, default='cifar100', help='location of the data corpus')
+parser.add_argument('--gamma',type=float,default=0.2,help='gamma')##
+parser.add_argument('--sparsity-regularization', '-sr', dest='sr', action='store_true',help='train with channel sparsity regularization')
+parser.add_argument('--s', type=float, default=0.0001,help='scale sparse rate (default: 0.0001)')
+best_prec1 = 0
+
+if not os.path.exists('./checkpoints'):
+    os.mkdir('./checkpoints')
+    
+def updateBN(model):
+    Op = model._modules.items()
+    for m in Op:
+        if m[0]=='layer1':
+            for m1 in m[1]:
+                #m1.bn_a.weight.grad.data.add_(args.s * torch.sign(m1.bn_a.weight.data))
+                m1.nbam.Channel_Att.bn2.weight.grad.data.add_(args.s * torch.sign(m1.nbam.Channel_Att.bn2.weight.data))
+                m1.nbam.Spatial_Att.bn.weight.grad.data.add_(args.s * torch.sign(m1.nbam.Spatial_Att.bn.weight.data))
+        if m[0]=='layer2':
+            for m2 in m[1]:
+                #m2.bn_a.weight.grad.data.add_(args.s * torch.sign(m2.bn_a.weight.data))
+                m2.nbam.Channel_Att.bn2.weight.grad.data.add_(args.s * torch.sign(m2.nbam.Channel_Att.bn2.weight.data))
+                m2.nbam.Spatial_Att.bn.weight.grad.data.add_(args.s * torch.sign(m2.nbam.Spatial_Att.bn.weight.data))
+        if m[0]=='layer3':
+            for m3 in m[1]:
+                #m3.bn_a.weight.grad.data.add_(args.s * torch.sign(m3.bn_a.weight.data))
+                m3.nbam.Channel_Att.bn2.weight.grad.data.add_(args.s * torch.sign(m3.nbam.Channel_Att.bn2.weight.data))
+                m3.nbam.Spatial_Att.bn.weight.grad.data.add_(args.s * torch.sign(m3.nbam.Spatial_Att.bn.weight.data))
+        if m[0]=='layer4':
+            for m4 in m[1]:
+                #m4.bn_a.weight.grad.data.add_(args.s * torch.sign(m4.bn_a.weight.data))
+                m4.nbam.Channel_Att.bn2.weight.grad.data.add_(args.s * torch.sign(m4.nbam.Channel_Att.bn2.weight.data))
+                m4.nbam.Spatial_Att.bn.weight.grad.data.add_(args.s * torch.sign(m4.nbam.Spatial_Att.bn.weight.data))
+
+def main():
+    global args, best_prec1
+    global viz, train_lot, test_lot
+    args = parser.parse_args()
+    print ("args", args)
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    random.seed(args.seed)
+
+    # create model
+    if args.arch == "resnet":
+        model = ResidualNet( 'CIFAR100', args.depth, 100, args.att_type )
+    
+    inputs = torch.randn(1, 3, 32, 32)
+    total_ops, total_params = profile(model, (inputs,), verbose=False)
+    print(" %.2f | %.2f" % ( total_params / (1000 ** 2), total_ops / (1000 ** 3)))
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                            momentum=args.momentum,
+                            weight_decay=args.weight_decay)
+    model = torch.nn.DataParallel(model, device_ids=list(range(args.ngpu)))
+    #model = torch.nn.DataParallel(model).cuda()
+    model = model.cuda()
+    #print ("model")
+    #print (model)
+    
+    train_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=args.milestones, gamma=args.gamma)##
+
+    # get the number of model parameters
+    print('Number of model parameters: {}'.format(
+        sum([p.data.nelement() for p in model.parameters()])))
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            if 'optimizer' in checkpoint:
+                optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+
+    cudnn.benchmark = True
+    
+    if args.set=='cifar100':
+        train_transform, valid_transform = data_transforms_cifar10(args)
+        train_data = datasets.CIFAR100(root=args.data, train=True, download=True, transform=train_transform)
+        valid_data = datasets.CIFAR100(root=args.data, train=False, download=True, transform=valid_transform)
+    else:
+        train_transform, valid_transform = data_transforms_cifar10(args)
+        train_data = datasets.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
+        valid_data = datasets.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
+        
+    train_loader = torch.utils.data.DataLoader(
+      train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=args.workers)
+
+    val_loader = torch.utils.data.DataLoader(
+      valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=args.workers)
+    
+    for epoch in range(args.start_epoch, args.epochs):
+        #adjust_learning_rate(optimizer, epoch)
+        
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch)
+        
+        # evaluate on validation set
+        prec1 = validate(val_loader, model, criterion, epoch)
+        
+        train_scheduler.step()
+        
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best, args.prefix)
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        target = target.cuda()
+        input_var = torch.autograd.Variable(input).cuda()
+        target_var = torch.autograd.Variable(target)
+        
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+        top5.update(prec5.item(), input.size(0))
+        
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        if args.sr and args.att_type=='NBAM':
+            updateBN(model)
+        optimizer.step()
+        
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+def validate(val_loader, model, criterion, epoch):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        target = target.cuda()
+        with torch.no_grad():
+            input_var = torch.autograd.Variable(input).cuda()
+            target_var = torch.autograd.Variable(target)
+        
+        # compute output
+            #output = model(input_var)
+            #loss = criterion(output, target_var)
+            
+            output = model(input_var)#,target_var
+            loss = criterion(output, target_var)
+        
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+        top5.update(prec5.item(), input.size(0))
+        
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        
+        if i % args.print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1, top5=top5))
+    
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+            .format(top1=top1, top5=top5))
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, prefix):
+    filename='./checkpoints/%s_checkpoint.pth.tar'%prefix
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, './checkpoints/%s_model_best.pth.tar'%prefix)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def data_transforms_cifar10(args):
+    CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+    CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+      ])
+    #if args.cutout:
+    #    train_transform.transforms.append(Cutout(args.cutout_length))
+
+    valid_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        ])
+    return train_transform, valid_transform
+
+def data_transforms_cifar100(args):
+    #CIFAR_MEAN = [0.5071, 0.4865, 0.4409]
+    #CIFAR_STD = [0.2673, 0.2564, 0.2762]
+    CIFAR_MEAN = [125.3/ 255.0, 123.0/ 255.0, 113.9/ 255.0] 
+    CIFAR_STD = [63.0/ 255.0, 62.1/ 255.0, 66.7/ 255.0] 
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+      ])
+    #if args.cutout:
+    #    train_transform.transforms.append(Cutout(args.cutout_length))
+
+    valid_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        ])
+    return train_transform, valid_transform
+
+
+if __name__ == '__main__':
+    main()
