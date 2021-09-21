c76abf4e1e7026578b8e09e499e840c818406001
Yichao Liu <40708986+Christian-lyc@users.noreply.github.com>
2021-09-21 16:23:54 +0200

    Update attention.py

--- MODELS/attention.py
+++ MODELS/attention.py
@@ -25,46 +25,12 @@ class Channel_Att(nn.Module):
         return x
 
 
-
-class Spatial_Att(nn.Module):
-    def __init__(self, shape):
-        super(Spatial_Att, self).__init__()
-        self.H=shape
-        self.W=shape
-        self.bn=nn.BatchNorm1d(self.H*self.W,affine=True)
-        
-
-    def forward(self, x):
-        batch,channel,H,W=x.size()
-
-        residual = x
-        
-        x=x.view(batch,channel,-1)
-        x = x.permute(0, 2, 1).contiguous()
-        x=self.bn(x)
-        x = x.permute(0, 2, 1).contiguous()
-        weight_bn = self.bn.weight.data.abs() / torch.sum(self.bn.weight.data.abs())
-        x = weight_bn* x
-        x=x.view(batch, channel, H, W)
-
-        x = torch.sigmoid(x) * residual #
-
-        return x
-
-
 class Att(nn.Module):
     def __init__(self, channels,shape, out_channels=None, no_spatial=True):
         super(Att, self).__init__()
         self.Channel_Att = Channel_Att(channels)
-        self.no_spatial = no_spatial
-        #self.Spatial_Att = Spatial_Att(shape)
-
+  
     def forward(self, x):
         x_out1=self.Channel_Att(x)
-        '''
-        if not self.no_spatial:
-            x_out2 = self.Spatial_Att(x_out1)
-        else:
-            x_out2 = x_out1
-        '''
+ 
         return x_out1  
